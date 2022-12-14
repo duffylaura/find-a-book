79bfb1b521585d76af25c585ea55ec080688ecd2
Coffee Pawz <lauralynneduffy@gmail.com>
2022-12-13 22:17:33 -0500

    Cleaning up backend

--- server/server.js
+++ server/server.js
@@ -1,28 +1,25 @@
 const express = require('express');
-const {ApolloServer} = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-
+const { authMiddleware } = require('./utils/auth');
 const {typeDefs, resolvers} = require('./schemas');
 const db = require('./config/connection');
-
-// const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001; 
 const app = express();
 const server = new ApolloServer ({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-// app.use(routes);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
@@ -35,7 +32,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
     })
   })
 };

512e73521146cd9348407b5955de4330202853c3
Coffee Pawz <lauralynneduffy@gmail.com>
2022-12-22 14:55:33 -0500

    trying to figure out auth.js and server.js issues

--- server/server.js
+++ server/server.js
@@ -39,3 +39,34 @@ const startApolloServer = async (typeDefs, resolvers) => {
   
 // Call the async function to start the server
   startApolloServer(typeDefs, resolvers);
+
+// var express = require('express');
+// var { graphqlHTTP } = require('express-graphql');
+// var { buildSchema } = require('graphql');
+
+// var schema = buildSchema(`
+//   type Query {
+//     ip: String
+//   }
+// `);
+
+// const loggingMiddleware = (req, res, next) => {
+//   console.log('ip:', req.ip);
+//   next();
+// }
+
+// var root = {
+//   ip: function (args, request) {
+//     return request.ip;
+//   }
+// };
+
+// var app = express();
+// app.use(loggingMiddleware);
+// app.use('/graphql', graphqlHTTP({
+//   schema: schema,
+//   rootValue: root,
+//   graphiql: true,
+// }));
+// app.listen(3001);
+// console.log('Running a GraphQL API server at localhost:4000/graphql');
